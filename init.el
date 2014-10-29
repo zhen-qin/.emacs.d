34ab88fd84eaa63a304c4712b6a38a261c9fbaf3
qinzhen <qinzhen001@bupt.edu.cn>
2014-10-29 10:37:54 +0800

    adding my local org file to the org file list

--- init.el
+++ init.el
@@ -163,8 +163,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bmkp-last-as-first-bookmark-file "~/.emacs.bmk")
- '(safe-local-variable-values (quote ((emacs-lisp-docstring-fill-column . 75) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby")))))
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/.bookmarks.el")
+ '(safe-local-variable-values (quote ((emacs-lisp-docstring-fill-column . 75) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby"))))
+ '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -175,3 +176,12 @@
 ;;; no-byte-compile: t
 ;;; End:
 (put 'erase-buffer 'disabled nil)
+
+(setq org-agenda-files (list "~/Documents/working/mygtd.org"
+                             "~/Documents/working/inbox.org"))
+
+(setq org-capture-templates
+  '(("t" "TODO" entry (file+headline "~/Documents/working/mygtd.org" "inbox")
+         "* %?\n ")))
+
+(global-set-key "\C-cm" 'org-capture)
